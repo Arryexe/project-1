2dddfdd8999d56f4856c1eb6c00b035ac4a23641
Arryexe <arianur098@gmail.com>
2019-02-24 21:53:31 -0800

    make a feature list company

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    
+	public function index() {
+		$companies = Company::all();
+
+		return view('companies.index', compact('companies'));
+	}
+
+}
---
+++ resources/views/companies/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<div class="card">
+		<div class="card-header">Company Listing</div>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>No.</td>
+					<th>Company Name</td>
+					<th>Email</td>
+					<th>Website</td>
+					<th>Address</td>
+				</tr>
+			</thead>
+			<tbody>
+
+				<?php 
+					$no = 1;
+				?>
+				<?php foreach ($companies as $company): ?>
+					<tr>
+						<td>{{ $no++ }}</td>
+						<td>{{ $company->name }}</td>
+						<td>{{ $company->email }}</td>
+						<td>{{ $company->website }}</td>
+						<td>{{ $company->address }}</td>
+					</tr>
+				<?php endforeach ?>
+			</tbody>
+		</table>
+	</div>
+
+@endsection
